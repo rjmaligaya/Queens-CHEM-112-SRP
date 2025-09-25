11d4308e65ed9b52bea5fd6f81456d5dde048eb4
rjmaligaya <rjmaligaya@gmail.com>
2025-09-25 16:22:03 -0400

    Delete private directory

--- private/config.js
+++
@@ -1,8 +0,0 @@
-window.CHEM112_CONFIG = {
-  ALLOWED_WEEKS: [4,5,6,7,8,10],
-  REQUIRE_DIGITS: 8,
-  ENDPOINT: 'node', // we’ll use the 'node' slot for the Worker route
-  ENDPOINTS: {
-    node: '/api/ingest' // Worker route bound to your domain
-  }
-};
